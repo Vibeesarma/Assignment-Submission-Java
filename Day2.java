3d8d8bef1cc0f4b6b662d5561a59471abb7af9b3
Vibeesarma <77588716+vibeeshanan@users.noreply.github.com>
2021-09-03 23:59:49 +0530

    Day2 Assignment submitted

---
+++ Day2.java
@@ -0,0 +1,13 @@
+
+class Day2{
+
+     public static void main(String[] args) 
+     {
+         int x = Integer.parseInt(args[0]);
+         int y = Integer.parseInt(args[1]);
+         int sum =x+y;
+
+         System.out.println("Sum of two inputs : "+sum);
+     }
+    
+}
